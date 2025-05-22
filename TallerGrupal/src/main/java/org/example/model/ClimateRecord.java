ddf0516f54133f8835be56ee07cf342245cced74
ericroco <103292880+ericroco@users.noreply.github.com>
2025-05-21 20:35:18 -0500

    Delete TallerGrupal directory

--- TallerGrupal/src/main/java/org/example/model/ClimateRecord.java
+++
@@ -1,7 +0,0 @@
-package org.example.model;
-
-import java.time.LocalDateTime;
-
-public record ClimateRecord(LocalDateTime dateTime, double temp, double humidity, double windSpeed, double visibility, double pressure) {
-
-}
